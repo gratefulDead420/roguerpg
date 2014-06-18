2a68b4db2c77c22a6c8f59ca3ae019ae052c8be1
gratefulDead420 <tasmith1990@gmail.com>
2014-06-17 20:27:29 -0400

    Update world.php

--- world.php
+++ world.php
@@ -1,114 +1,74 @@
 <?php
 
 /*
-* world.php
+* script: world.php verson 1.0
 * developed by gratefulDeadty
+*
+* Todo: Error checks, create a clas for world to handle everything, and implement rooms based off logged in user.
 */
 
-require 'config.php';
+require 'core/init.php';
 
-class World
+$stmt = $dbh->prepare('SELECT id,roompic,north,east,south,west,name FROM world WHERE id=?');
+$stmt->bindValue(1, $_GET['room']);
+$stmt->execute();
+$room_check = $stmt->rowCount();
+
+if ($room_check == 0)
 {
-	private $dbh;
-	
-	public function __construct($database)
-	{
-		$this->dbh = $database;
-	}
-
-	public function getRoom($roomid)
-	{
-		$query = $this->dbh->prepare('SELECT * FROM `world` WHERE `id`=?');
-		$query->bindValue(1, $roomid);
-		$query->execute();
-		return $query->fetchAll(PDO::FETCH_ASSOC);
-	}
-	
-	public function getWorld($user_roomid)
-	{
-		$query = $this->dbh->query('SELECT * FROM `world` WHERE `id`=?');
-		$query->bindValue(1, $user_roomid);
-		$query->execute();
-		return $query->fetch();
-	}
-	
-	public function insertMovelog($userid, $ip, $userip, $roomid, $last)
-	{
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$query = $this->dbh->prepare("INSERT INTO `move_log` (`userid`, `ip`, `userip`, `roomid`, `last`) VALUES (?, ?, ?, ?, ?) ");
-		$query->bindValue(1, $userid);
-		$query->bindValue(2, $ip);
-		$query->bindValue(3, $userip);
-        	$query->bindValue(4, $roomid);
-        	$query->bindValue(5, $last);
-	}
-		
-	public function updateMove($roomid, $userid)
-	{	
-		$query = $this->dbh->prepare('UPDATE `users` SET `room`=? WHERE');
-		$query->bindValue(1, $roomid);
-		$query->bindValue(2, $userid);
-		$query->execute();
-	}
-	
-	public function getMobs($roomid)
-	{
-		$spawn_mob_time = 0;
-		$query = $this->dbh->prepare('SELECT * FROM `mobs` WHERE `room`=? AND `spawn_mob_time`<=? ');
-		$query->bindValue(1, $roomid);
-		$query->bindValue(2, $spawn_mob_time);
-		$query->execute();
-		return $query->fetchAll(PDO::FETCH_ASSOC);
-	}
-	
+        die('<div class="error_msg">Error: That room does not exist.</div>');
 }
-	//errors array.
-	$errors = array();
-	if (empty($errors) === false)
-	{
-		echo ' '.implode($errors).' ';
-	}
 
-	$user_roomid = $user['room'];
-	$world_main = new World($dbh);
-	$rooms = $world_main->getRoom(isset($_GET['room']));
-	$worlds = $world_main->getWorld($user_roomid);
-	
-	/* error checks. */
-	if (empty($_GET['room']) === true)
-	{
-		$errors[] = 'Error: Invalid room. (error1)';
-	}
-	if (!is_numeric($_GET['room']))
-	{
-		$errors[] = 'Error: Invalid room (error2).';
-	}
-	if (empty($_GET['room'] === false)
-	{
-		if (count($world['id']) == 0)
-		{
-			$errors[] = 'Error: This room does not exist.';
-		}
-		
-		foreach ($rooms as $room)
-		{
-			if ($room['id'] != $user_roomid)
-			{
-				if ($room['north'] != $user_roomid  AND $room['south'] != $user_roomid AND $room['west'] != $user_roomid AND $room['east'] != $user_roomid)
-				{
-					$errors[] = 'Error: You\'re not next to this room';
-				}
-		}
-		$userid = $user['id'];
-		$userip = $user['ip'];
-		$roomid = $room['id'];
-		$user_roomid = $user['room'];
-		$insert_move = $world_main->insertMovelog($userid,$ip,$userip,$roomid,$last);
-		$update_room = $world_main->updateMove($roomid,$userid);
-		?>
-		<!-- start pageUpdate ajax load. -->
+if ($_GET['room'] && empty($errors) === true)
+{
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $room)
+        {        
+                $north = $room['north'];
+                $south = $room['south'];
+                $east = $room['east'];
+                $west = $room['west'];
+
+                if ($north > 0)
+                {
+	                $north = $north;
+                }
+                else
+                {
+	                $north = $_GET['room'];
+                }
+ 
+                if ($south > 0)
+                {
+	                $south = $south;
+                }
+                else
+                {
+	                $south = $_GET['room'];
+                }
+
+               if ($east > 0)
+                {
+	                $east = $east;
+                }
+                else
+                {
+	                $east = $_GET['room'];
+                }
+
+                if ($west > 0)
+                {
+	                $west = $west;
+                }
+                else
+                {
+	                $west = $_GET['room'];
+                }
+
+
+                ?>
+                <!-- start keyboard handler and ajax load. -->
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<script type="text/javascript">
-		//keyboard usage. for wasd keys.
 		var key1="87";  //W
 		var key2="119"; //w
 		var key3="83";  //S
@@ -119,22 +79,22 @@ class World
 		var key8="97";  //a
 		var x='';
 		
-		//.ajax function that will create our new room load.
+		//.ajax function that loads the new room.
 		//in the future, i'd like to make this a websocket load.
-		function pageUpdate(roomid)
-		{
-			$('#divsp').html("<img src='images/ajaxloading.gif'>");
-			var grbData = $.ajax(
-			{
-				type: "GET",
-				cache: false,
-				url: "world.php?room="+roomid,
-				success: function (html)
-				{
-					$("yourhugediv").html(html);
-				}
-			});
-		}	
+                function pageUpdate(roomid) 
+                { 
+                        $('#world_loading').html("<img src='images/ajaxloading.gif'>");
+                        var grbData = $.ajax({
+                        type: 'GET',
+		        cache: false,
+                        url : "http://tuts.site40.net/rpg/world.php?room="+roomid,
+                        success: function (html) 
+                        {
+                                $("#world_create").html(html);
+                        }
+                        });
+                }
+
 		function handleKeyboard(evt)
 		{
 			evt=(evt)?evt:((window.event)?event:null);
@@ -142,27 +102,27 @@ class World
 			{
 				if(evt.keyCode==87)
 				{
-					pageupdate('<?php echo ' '.$room['north'].' ' ?>');
+					pageUpdate('<?php echo ' '.$north.' ' ?>');
 				}
 				if(evt.keyCode==68)
 				{
-					pageupdate('<?php echo ' '.$room['east'].' '; ?>');
+					pageUpdate('<?php echo ' '.$east.' '; ?>');
 				}
 				if(evt.keyCode==65)
 				{ 
-					pageupdate('<?php echo ' '.$room['west'].' '; ?>');
+					pageUpdate('<?php echo ' '.$west.' '; ?>');
 				}
 				if(evt.keyCode==83)
 				{
-					pageupdate('<?php echo ' '.$room['south'].' '; ?>');
+					pageUpdate('<?php echo ' '.$south.' '; ?>');
 				}
 			}
 		}
 		document.onkeyup = handleKeyboard;
 		</script>
-		
 <!-- begin page content. -->
-<div id="yourhugediv">
+<div align='center'>
+<div id="world_create">
 	<table border="0" cellspacing="1" cellpadding="1" width="50%" bgcolor="#000000">
 	<tr>
 		<td colspan="2" bgcolor="#333333">
@@ -175,19 +135,14 @@ class World
 			<table width="155" valign="top" bgcolor="#333333">
 	<tr>
 		<td width="155" style="border:1px solid #000000;" bgcolor="#333333" COLSPAN=3>
-				<img border="0" src="<?php echo ' '.$room['roompic'].' ';?>" usemap="#map"/></td>
+				<img border="0" src="images/<?php echo ''.$room['roompic'].'';?>" usemap="#map"/></td>
 	</tr>
 	<tr>
 	<tr style="border: 1px solid #000000; border-collapse: collapse;" bgcolor="#333333"><td width="250"><center>
 
 <?php
 
-$north = $room['north'];
-$west = $room['west'];
-$east = $room['east'];
-$south = $room['south'];
-
-/* left,right,up,down image links. */
+//left,right,up,down image links.
 echo '<tr bgcolor="#333333"><td width="250" style="border: 1px solid #000000;" bgcolor="#2b2b2b"><center>';
 
 if ($north > 0) 
@@ -208,7 +163,7 @@ else
 	echo '<img src="images/arrowWa.png">';
 }
 
-echo '<span id="divsp"></span>'; //loading image from the ajax request.
+echo '<span id="world_loading"></span>'; //loading image from the ajax request.
 
 if ($east > 0) 
 {
@@ -228,6 +183,9 @@ else
 	echo '<img src="images/arrowSa.png">';
 }
 
+
+}
+
 ?>
 
 <!-- Mobs -->
@@ -240,65 +198,29 @@ else
 	<tr>
 		<td bgcolor="#2b2b2b" style="border:1px solid #000000;"><center><strong>Creatures in this room:</strong></td>
 		<?php
-		$mobs = $world_main->getMobs(isset($_GET['room']));
-		foreach ($mobs as $mob)
+		$stmt = $dbh->prepare('SELECT * FROM `roommobs` WHERE `roomid`=? ');
+		$stmt->bindValue(1, $_GET['room']);
+		$stmt->execute();
+		foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $mob)
 		{	
 			$mobid = $mob['id'];
 			$mobname = $mob['name'];
-			$moblevel = $mob['level'];
 			echo '<tr><td>';
-		
-			$timeleft = 0;
-			$stmt = $dbh->prepare('SELECT * FROM dead_mobs WHERE ' . 'mid=:mid AND ' . 'timeleft>:timeleft AND ' . 'uid=:uid ');
-			$stmt->bindParam('mid', $mobid);
-			$stmt->bindParam('timeleft', $timeleft);
-			$stmt->bindParam('uid', $userid);
-			$stmt->execute();
-			$count = $stmt->rowCount();
-			if ($count > 0)
-			{
-				echo '';
-			}
-			else
-			{
-				if (empty($mob['type']) === true)
-				{
-					$mobtitle = 'viewmob';
-				}
-				if ($mob['type'] == 'Q')
-				{
-					$mobtitle = 'viewquest';
-				}
-				if ($mob['type'] == 'T')
-				{
-					$mobtitle = 'viewtrainer';
-				}
-				?>
-	<div style="border-bottom:1px dotted; border-color:#000000; height:15px;"> 
-    <a href="mobattack.php?mob=<?php echo ' '.$mobid.' '; ?>">
-	<img align="right" border="0" alt="Attack!" src="images/attackplayericon.jpg" style="cursor:pointer;" onMouseOver="menutip('Attack <?php echo ' '.$mob['name'].' '; ?>!');" onMouseOut="hideddrivetip();"> 
-	<a href="<?php echo ' '.$mobtitle.' ';?>.php?mob=<?php echo ' '.$mobid.' '; ?>" style="cursor:pointer;">
-		
-	<?php
-	if ($mob['type'] == 'Q')
-	{
-		echo "<img align='right' src='images/talk_icon.jpg' border='0' onMouseOver=\"menutip('$mobname has a quest for you!');\" onMouseOut=\"hideddrivetip();\"></a>";   
-		echo '<a href="'.$mobtitle.'.php?mob='.$mobid.'" style="cursor:pointer;">';
-	}
-	if ($mob['type'] == 'T')
-	{
-		echo "<img align='right' src='images/world/trainer.jpg' border='0' onMouseOver=\"menutip('$mobname is a trainer!');\" onMouseOut=\"hideddrivetip();\"></a>";
-		echo '<a href="'.$mobtitle.'.php?mob='.$mobid.'" style="cursor:pointer;" ONMOUSEOVER="menutip(Level $moblevel)" ONMOUSEOUT="hideddrivetip()">';
-		echo '<font color="#949477">&nbsp;'.$mobname.' ('.$moblevel.')&nbsp;</font></a></div>';
-	}
-	    echo '</div>';
-	
-	echo '</tr></table>';
+                        $mobtitle = 'viewmobs';
+		?>
+	                <div style="border-bottom:1px dotted; border-color:#000000; height:15px;"> 
+                        <a href="mobattack.php?mob=<?php echo ' '.$mobid.' '; ?>"><?php echo ' '.$mobname.' '; ?>
+	                <img align="right" border="0" alt="Attack!" src="images/attackplayericon.jpg" style="cursor:pointer;" onMouseOver="menutip('Attack <?php echo ' '.$mob['name'].' '; ?>!');" onMouseOut="hideddrivetip();"> 
+	                <a href="<?php echo ' '.$mobtitle.' ';?>.php?mob=<?php echo ' '.$mobid.' '; ?>" style="cursor:pointer;">
+	                </div>
+                <?php
+                }
+                ?>
+	</tr>
+   </table>
+</div>
+<?php 
 
-
-		}
-	} 
-	
-	}
 }
-	
+
+?>
